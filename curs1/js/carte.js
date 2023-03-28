95291a8a0900fbdc61ce4b7a13fba4c5bb273b36
Frontend JavaScript <frontendjavascript@C3-10.local>
2023-03-28 20:47:22 +0300

    curs

--- curs1/js/carte.js
+++ curs1/js/carte.js
@@ -1,10 +1,30 @@
 class Carte{
-    constructor(titlu, autor, editura, an, pret){
+    static reducere = 10;
+    mypret;
+    constructor(titlu, autor, editura, an, nrpagini){
         this.titlu = titlu;
         this.autor = autor;
         this.editura = editura;
         this.an = an;
-        this.pret = pret;
+       
+        this.nrpagini = nrpagini;
+    }
+    get pret(){
+        console.log("sunt in getter");
+        return this.mypret;
+    }
+    set pret(pretNou){
+        console.log("sunt in setter");
+        if (pretNou <= 0){
+            throw "pretul nu poate fi actualizat";
+        }
+        else{
+            this.mypret= pretNou;
+        }
+        // this.pret = pretNou;
+    }
+    info(){
+        console.log(`titlul este  ${this.titlu}, autor ${this.autor}`);
     }
     deschideCartea(pagina){
         console.log(`am deschis cartea la pagina ${pagina}`);
@@ -12,13 +32,22 @@ class Carte{
     randomIntFromInterval(min, max){
         return Math.floor(Math.random() * (max-min+1) +min)
     }
-    modificaPret(pretNou){
-        if(pretNou<100 && pretNou>this.pret){
-            this.pret =pretNou;
-        }
-    }
+    // modificaPret(diferenta){
+    //     if(pretNou<100 && pretNou>this.pret){
+    //         this.pret =pretNou;
+    //     }
+    // }
     reducere(procent){
         this.pret = this.pret - (procent/100 * this.pret)
         console.log(` pretul cu reducere este ${this.pret}`)
     }
+
+
+    // pretNou(){
+    //     this.pret = this.pret - (Carte.reducere * this.pret) / 100
+    // }
+
+    // modificaPret(diferenta){
+    //     this.pret = this.pret - diferenta;
+    // }
 }
